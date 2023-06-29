fa533d87cdb6d50f5c863865faac252d332972d5
PShivers <paulsshivers@gmail.com>
2023-06-29 14:06:28 -0400

    info modal added

---
+++ src/components/modal.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Modal = ({ closeModal }) => {
+	return (
+		<div className="fixed top-0 left-0 w-full h-full flex items-center justify-center modal-overlay drop-shadow-xl backdrop-blur-sm	">
+			<div className="bg-white p-6 rounded max-w-sm modal">
+				<h3 className="text-xl font-bold mb-4">How To Play</h3>
+				<p className="mb-4">
+					Lexiplex is a word puzzle game where you are given a clue and the
+					answer will be made of 3 words, with two of the words making up the
+					third.
+				</p>
+				<p className="mb-4">
+					For example: If the clue is <b>"That handlebar has gotta hurt!"</b>{" "}
+					the answer would be <b>"Mustache Must Ache"</b>.
+				</p>
+				<p className="mb-4">
+					If you are stuck and need a hint simply click any of the letters on
+					the bottom of the screen and you will see any occurences of the letter
+					in the blank tiles Wheel of Fortune style.
+				</p>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3"
+					onClick={closeModal}
+				>
+					Got it!
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { useState } from "react";
+import Modal from "../components/modal"; // Import your modal component here
+
 const puzzles = require("../../utils/puzzles");
+
 // TODO: add check on submit to see if it is right
 // TODO: ADD "HOW TO" MODAL
 // TODO: add logic when clicking btn to check if puzzle isSolved
@@ -26,6 +29,12 @@ const logoStyles = {
 //#endregion Styles
 
 const IndexPage = () => {
+	const [isModalVisible, setModalVisible] = useState(false);
+
+	const handleSVGClick = () => {
+		setModalVisible(true);
+	};
+
 	const [puzzle, setPuzzle] = useState(
 		puzzles[Math.floor(Math.random() * puzzles.length)]
 	);
@@ -86,7 +95,7 @@ const IndexPage = () => {
 				class={
 					disabledButtons.includes(letter)
 						? "bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 m-1"
-						: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
+						: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded m-1 drop-shadow-xl"
 				}
 				onClick={() => handleLetterClick(letter)}
 				disabled={disabledButtons.includes(letter)}
@@ -117,16 +126,40 @@ const IndexPage = () => {
 
 	return (
 		<main style={pageStyles}>
-			<h1 className="text-3xl	mb-3 md:text-8xl md:mb-8">
-				Lexi<span style={logoStyles}>plex</span>
-			</h1>
+			<header className="w-full text-center flex flex-row items-center justify-between mb-3 md:mb-8 md:justify-center border-bottom-solid border-2 border-black border-x-0 drop-shadow-lg">
+				<div className="grow-[1]"></div>
+				<h1 className="text-5xl md:text-8xl grow-[2] drop-shadow-lg	">
+					Lexi
+					<span className="text-tomato" style={logoStyles}>
+						plex
+					</span>
+				</h1>
+				<div className="flex items-center grow-[1]">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="w-6 h-6 ml-auto cursor-pointer"
+						onClick={handleSVGClick}
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+						/>
+					</svg>
+				</div>
+			</header>
+
 			<h2 className="text-xl md:text-3xl">Today's clue:</h2>
 			<p className="text-center text-xl	md:text-3xl mb-3">{puzzle.clue}</p>
 			<div className="flex flex-wrap justify-center mt-3">{tiles}</div>
 			<form className="w-full max-w-sm mb-3 mt-3">
 				<div className="flex items-center border-b border-slate-300 py-2">
 					<input
-						className="appearance-none bg-transparent border-none w-full text-black-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+						className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
 						type="text"
 						placeholder="Your answer here..."
 						aria-label="Answer"
@@ -134,7 +167,10 @@ const IndexPage = () => {
 				</div>
 			</form>
 			<p>Hints used: {hintCount}</p>
-			<div className="flex flex-wrap shrink justify-center mt-3">{letters}</div>
+			<div className="flex flex-wrap shrink justify-center mt-3 ml-3 mr-3">
+				{letters}
+			</div>
+			{isModalVisible && <Modal closeModal={() => setModalVisible(false)} />}
 		</main>
 	);
 };
