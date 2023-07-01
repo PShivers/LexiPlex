6c552e795f1b74392bfcbd55ca6c00e9b50a919d
PShivers <paulsshivers@gmail.com>
2023-07-01 12:28:30 -0400

    fixed icon spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -110,7 +110,11 @@ const IndexPage = () => {
 	//#endregion
 
 	//#region handlers
-	const handleSVGClick = () => {
+	const handleMenuClick = () => {
+		console.log("menu clicked");
+	};
+
+	const handleInfoClick = () => {
 		setModalVisible(true);
 	};
 
@@ -158,22 +162,25 @@ const IndexPage = () => {
 
 	return (
 		<main style={pageStyles} className="flex flex-col items-center md:p-5">
-			<header className="w-full text-center flex flex-row items-center justify-between mb-3 md:mb-8 md:justify-center border-bottom-solid border-2 border-slate-600 border-x-0 drop-shadow-lg px-2">
-				<div className="flex items-center grow-[1]">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6 w-full"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-						/>
-					</svg>
+			<header className="w-full text-center flex flex-row items-center justify-between mb-3 md:mb-8 md:justify-center border-bottom-solid border-2 border-slate-600 border-x-0 drop-shadow-lg px-4 md:px-7">
+				<div className="flex items-center justify-start grow-[1]">
+					<div className="svg-wrapper">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							stroke="currentColor"
+							className="w-6 h-6 w-full cursor-pointer"
+							onClick={handleMenuClick}
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+							/>
+						</svg>
+					</div>
 				</div>
 				<h1 className="text-5xl md:text-8xl grow-[2] drop-shadow-lg	">
 					Lexi
@@ -181,22 +188,24 @@ const IndexPage = () => {
 						plex
 					</span>
 				</h1>
-				<div className="flex items-center grow-[1]">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth={1.5}
-						stroke="currentColor"
-						className="w-6 h-6 cursor-pointer w-full"
-						onClick={handleSVGClick}
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
-						/>
-					</svg>
+				<div className="flex items-center justify-end grow-[1]">
+					<div className="svg-wrapper">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							className="w-6 h-6 w-full cursor-pointer"
+							onClick={handleInfoClick}
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+							/>
+						</svg>
+					</div>
 				</div>
 			</header>
 			<h2 className="text-2xl md:text-3xl">Today's clue:</h2>
